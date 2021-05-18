6cb08fedb58ca7c93cdcf338953c1b315618b86a
aycadamar <ayca.damar@gmail.com>
2021-05-18 22:14:43 +0300

    inheritance2

--- Day2Hw1/src/Day2Hw1/CourseManager.java
+++ Day2Hw1/src/Day2Hw1/CourseManager.java
@@ -3,8 +3,9 @@ package Day2Hw1;
 public class CourseManager {
 
 	public void addCourse(Course course) {
-		System.out.println(course.courseName + "is added to your account...");
+		System.out.println(course.courseName + " is added to your account...");
 	}
+	
 	public void deleteCourse(Course course) {
 		System.out.println(course.courseName +" is deleted from your account...");
 	}
--- Day2Hw1/src/Day2Hw1/Main.java
+++ Day2Hw1/src/Day2Hw1/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		Course course1 = new Course(1,"Yazýlým Geliþtirici Yetiþtirme Kampý (JAVA + REACT)","2 ay sürecek Java-React Kampý","Engin Demirog");
-		Course course2 = new Course(2,"Yazýlým Geliþtirici Yetiþtirme Kampý (C# + ANGULAR)","2 ay sürecek C# - Angular Kampý","Engin Demirog");
+		Course course1 = new Course(1,"Yazýlým Geliþtirici Yetiþtirme Kampý (JAVA + REACT)", "2 ay sürecek Java-React Kampý", "Engin Demiroð");
+		Course course2 = new Course(2,"Yazýlým Geliþtirici Yetiþtirme Kampý (C# + ANGULAR)", "2 ay sürecek C# - Angular Kampý", "Engin Demiroð");
 		Course course3 = new Course(3,"Programlamaya Giriþ için Temel Kurs","Giriþ seviye kurs","Engin Demirog");
 
 		Course[] courses = {course1,course2,course3};
 		
-
+		
         Instructor instructor1 = new Instructor(1, "Engin","Demiroð", "MCT,PMP,ITIL");
         
         Instructor[] instructors = {instructor1};
